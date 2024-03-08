a1eb4fbcbbfbed8fb907b190c8d6fffca44b308d
rapidmon <ehhwll@hanmail.net>
2024-03-08 16:16:10 +0900

    유튜브 영상 임베드 되는지 확인 중 코드

--- index.js
+++ index.js
@@ -13,8 +13,6 @@ window.onload = function() {
 let player;
 let Submit = false;
 
-videolist = ['x-Om0G8Dwis', 'l-jZOXa7gQY', 'kmD6JcAV_Rc', 'GAy1NSzjxYY', 's4Ow55AbdCg', 'JDh0CtosCXU']
-
 // youtube iframe api 호출 후 player 설정 함수
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
@@ -24,9 +22,10 @@ function onYouTubeIframeAPIReady() {
             'autoplay': 0,    // 자동 재생 비활성화
             'controls': 1,    // 컨트롤 바 표시
             'rel': 0,         // 관련 동영상 표시 안 함
-            'listType':'playlist',
-            'list': 'PLpLT-NtaAE5izfhXx7YISgKEPVU_PMM0D' // 재생목록 ID
+            // 'listType':'playlist',
+            // 'list': 'PLpLT-NtaAE5izfhXx7YISgKEPVU_PMM0D' // 재생목록 ID
         },
+        videoId: 'J2QiLaUqXfY',
         events: {
             'onReady': onPlayerReady,
             'onStateChange': onPlayerStateChange
@@ -36,8 +35,8 @@ function onYouTubeIframeAPIReady() {
 
 // 플레이어 준비가 완료되면 호출될 함수
 function onPlayerReady() {
-    player.setShuffle(true);
-    player.playVideoAt(0);
+    // player.setShuffle(true);
+    // player.playVideoAt(0);
     document.getElementById('startGame').addEventListener('click', function() {
         player.seekTo(0);
         player.playVideo();
